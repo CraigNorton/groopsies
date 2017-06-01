75f49440ff8a9c14483c0d58104ca0e9f70be5ac
Craig Norton <craigbnorton@gmail.com>
2017-05-31 21:15:06 -0400

    first push and initial code to play with

---
+++ widgchat/js/main.js
@@ -0,0 +1,19 @@
+$(document).on("click","#make-post",function(){
+  var postInput = $("post-input");
+  alert("hello");
+})
+$(document).on("click","#submit-chat-form",function(){
+  alert("hello");
+  var profilePic = $("#chat-profile-input").val();
+  var name = $("#chat-name-input").val();
+  var bio = $("#chat-bio-input").val();
+  var privacy = $("#chat-privacy-input").val();
+  var purpose = $("#chat-pu-input").val();
+  var posting = $("#chat-posting-input").val();
+  alert("goodbye");
+});
+var yearString = "<option>Year</option>";
+for(var i = 1950; i <= 2025; i++){
+  yearString += "<option value='" + i + "'>" + i + "</option>";
+}
+$("#DOBYear").html(yearString);
